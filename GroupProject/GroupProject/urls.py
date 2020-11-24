0365895c5e68630c9b269982cc88c10e25d69ce6
itachikryst <44116634+itachikryst@users.noreply.github.com>
2020-11-24 20:35:24 +0100

    Html, structure changes, url handling etc.

--- GroupProject/GroupProject/urls.py
+++ GroupProject/GroupProject/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from pages.views import homepage_view
 
 urlpatterns = [
-    path('index/', include('index.urls')),
-    path('client/', include('client.urls')),
-    path('workshop/', include('workshop.urls')),
+    path('', homepage_view, name='home'),
+    #path('index/', include('index.urls')),
+    #path('client/', include('client.urls')),
+    #path('workshop/', include('workshop.urls')),
     path('admin/', admin.site.urls),
 ]
 
